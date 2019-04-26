6cf85daeb4d5f354c3bcd25feee87059fcd3759f
Jonatan Piccoli Pazza <jonatan.pazza@gmail.com>
2019-04-26 09:09:17 -0300

    teste 1

---
+++ Hello/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+            Console.WriteLine("teste");
+            Console.ReadKey();
+        }
+
+        public static String createMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ HelloTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello;
+
+namespace HelloTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals("Hello World", Program.createMessage());   
+        }
+    }
+}
